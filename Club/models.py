a9a29d79908f2394759db3716d820a596ebed458
purva <purvajg@gmail.com>
2020-02-03 00:33:18 -0800

    Final Commit Assignment 5

--- Club/models.py
+++ Club/models.py
@@ -30,7 +30,10 @@ class Resource(models.Model):
     dateEntered=models.DateField()
     userID=models.ForeignKey(User, on_delete=models.DO_NOTHING)
     description=models.TextField()
+    resourceManager = models.Manager()
     
+    def __str__(self):
+        return self.resourceName
     class Meta:
         db_table='resource'
         verbose_name_plural='resource'
--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 def index (request):
     return render(request, 'clubApp/index.html')
+
+def getResources(request):
+    resource_list= Resource.resourceManager.all()
+    return render(request, 'clubApp/resource.html', {'resource_list' : resource_list})
